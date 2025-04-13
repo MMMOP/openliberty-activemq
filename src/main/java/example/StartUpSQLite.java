c56e08239f3b514d827ae64ed6558c72dbf0f463
theerasun.t <theerasun@gmail.com>
2025-04-13 15:34:11 +0700

    connect SQLite Database.

---
+++ src/main/java/example/StartUpSQLite.java
@@ -0,0 +1,36 @@
+package example;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@Singleton
+@Startup
+public class StartUpSQLite {
+
+    @Resource(lookup = "jdbc/sqlite")
+    private DataSource dataSource;
+
+    @PostConstruct
+    public void init() {
+        try (Connection conn = dataSource.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT 'temp1' as name " +
+                                                    "UNION ALL " +
+                                                    "SELECT 'temp2'" +
+                                                    "UNION ALL " +
+                                                    "SELECT 'temp3'")) {
+
+            while (rs.next()) {
+                System.out.println("User: " + rs.getString( "name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
